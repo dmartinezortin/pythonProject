de47da501f4ab2093e7f837f5cce602d08d2f5f0
Daniel <dmartinez@institutmvm.cat>
2021-01-21 12:30:22 +0100

    commit

--- exercici12.py
+++ exercici12.py
@@ -4,23 +4,15 @@ def main():
 
     #Demana els numeros i els introdueix a la llista
     for x in range(10):
-        main_list.insert(x, int(input("Introdueix un numero: ")))
+        main_list.append(int(input("Introdueix un numero: ")))
         while main_list[x] >10 or main_list[x]<0:
             main_list.insert(x, int(input("Introdueix un numero dins del rang! (0-10): ")))
 
+    #Separa els parells dels imparells
+    pair_list = [x for x in range(10) if main_list[x] % 2 == 0]
+    odd_list = [x for x in range(10) if main_list[x] % 2 != 0]
 
-
-    slice_obj = slice(0, 9, 1)
-    pair_list = list(main_list[slice_obj])
-    odd_list = list(main_list[slice_obj])
-
-    for x in range(10):
-        if main_list[slice_obj] % 2 == 0:
-            pair_list = main_list[slice_obj]
-
-
-
-
+    #Printa parells i imparells
     print(pair_list[:])
     print(odd_list[:])
 if __name__ == '__main__':
