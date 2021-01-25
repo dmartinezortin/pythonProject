49c9bff9844f53306985d4be18eba74394077db1
Daniel <dmartinez@institutmvm.cat>
2021-01-25 12:25:31 +0100

    commit

--- exercici4.py
+++ exercici4.py
@@ -1,10 +1,28 @@
 def main():
-    aux = 1
-    for x in range(1, 8):
-        slice_obj = slice(1, x-1, -1)
-        str_aux = str(aux)
-        print(aux  if aux ==1 else aux, str_aux[slice_obj])
-        aux = ((x+1)*10) +aux
+    value = 7
+    for i in range(value1 + 1):
+        space = 1
+        for space in range(value1 - i):
+            print(" ", end="")
+        j = i
+        for j in range(j, 0, -1):
+            print(j, end="")
+        j = 2
+        for j in range(j, i + 1):
+            print(j, end="")
+        print("")
+    for i in range(value1 - 1, 0, -1):
+        space = 1
+        for space in range(value1 - i):
+            print(" ", end="")
+        j = i
+        for j in range(j, 0, -1):
+            print(j, end="")
+        j = 2
+        for j in range(j, i + 1):
+            # if j != 0:
+            print(j, end="")
+        print("")
 
 if __name__ == '__main__':
         main()
