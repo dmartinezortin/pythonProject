d4668a29bafb70a86dcbf956b87ceadc81211a57
Daniel <dmartinez@institutmvm.cat>
2021-01-28 10:38:23 +0100

    commit2

--- exercici13.py
+++ exercici13.py
@@ -1,15 +1,11 @@
 def main():
     #Introduir valors
-    value1 = input("Introdueix el primer numero: ")
-    value2 = input("Introdueix el segon numero: ")
-    #Conversió de valors
-    size = len(value1)
-    value1 += value2
-    value2 = value1[0:size]
-    value1 = value1[size:]
-    int(value1)
-    int(value2)
-    #Printar valors
-    print("Valor 1: " + value1 + " Valor2: " + value2)
+    value1 = int(input("Introdueix el primer numero: "))
+    value2 = int(input("Introdueix el segon numero: "))
+    aux = value1
+    value1 = value2
+    value2 = aux
+    print("Valor 1: " , value1 , "Valor 2: " , value2)
+
 if __name__ == '__main__':
         main()
