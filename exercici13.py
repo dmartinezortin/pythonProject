fe1da9473a06ecfb5f3325a0815403fd8a85bd0c
Daniel <dmartinez@institutmvm.cat>
2021-01-28 10:32:04 +0100

    commit

--- exercici13.py
+++ exercici13.py
@@ -1,5 +1,4 @@
 def main():
-
     value1 = input("Introdueix el primer numero: ")
     value2 = input("Introdueix el segon numero: ")
     size = len(value1)
@@ -9,6 +8,5 @@ def main():
     int(value1)
     int(value2)
     print("Valor 1: " + value1 + " Valor2: " + value2)
-
 if __name__ == '__main__':
         main()
