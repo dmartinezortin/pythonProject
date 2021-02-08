72701f578d5d154459f1f1f5faeacc643b2158cc
Daniel <dmartinez@institutmvm.cat>
2021-02-08 10:14:45 +0100

    commit

--- exercici16.py
+++ exercici16.py
@@ -1,7 +1,7 @@
 def main():
     while True:
         value = int(input("Introdueix un numero del 1 al 20: "))
-        if value < 20 and value > 1:
+        if value <= 20 and value >= 1:
             break
     print([value*x for x in range(100) if (value * x) < 100])
 if __name__ == '__main__':
